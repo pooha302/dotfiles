741292a9709c570e09bd0b1ee16e4ff5a875bb38
하헌성 <pooha302@gmail.com>
2016-11-28 10:02:56 +0900

    oh-my-zsh update 체크 7일로 변경

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,6 +1,6 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
-export UPDATE_ZSH_DAYS=1
+export UPDATE_ZSH_DAYS=7
 
 ZSH_THEME="muse"
 plugins=(git fasd zsh-syntax-highlighting extract)
