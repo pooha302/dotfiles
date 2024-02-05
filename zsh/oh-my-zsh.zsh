18d32e44cee6db50bcee64cd64e8f16a1e915956
pooha302 <pooha302@gmail.com>
2024-02-05 11:50:51 +0900

    update

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -3,7 +3,7 @@ export ZSH=$HOME/.oh-my-zsh
 export UPDATE_ZSH_DAYS=7
 
 ZSH_THEME="muse"
-plugins=(git fasd zsh-syntax-highlighting extract zsh-autosuggestions)
+plugins=(git fasd zsh-syntax-highlighting extract)
 
 source $ZSH/oh-my-zsh.sh
 
