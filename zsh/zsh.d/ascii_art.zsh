22480d6fdfd0c0e57380e71a969a9ab59365607a
pooha302 <pooha302@gmail.com>
2026-02-12 08:20:00 +0900

    figlet using random font

--- zsh/zsh.d/ascii_art.zsh
+++ zsh/zsh.d/ascii_art.zsh
@@ -1,8 +1,12 @@
 [[ -o interactive ]] && command -v figlet > /dev/null 2>&1 || return
 
 local text="pooha302 / harock"
+local figlet_fonts=("doom" "drpepper" "ogre" "rectangles" "shadow" "small" "smkeyboard" "slant" "smslant" "speed" "trek")
+local random_font=${figlet_fonts[ $RANDOM % ${#figlet_fonts[@]} ]}
 if command -v lolcat > /dev/null 2>&1; then
-    figlet -w 200 "$text" | lolcat -f -p 1.0 -S $RANDOM
+    figlet -f $random_font -w 200 "$text" | lolcat -f -p 1.0 -S $RANDOM
 else
-    figlet -w 200 "$text"
+    figlet -f $random_font -w 200 "$text"
 fi
+
+print "\033[0;32mfiglet font >\033[1;33m $random_font \033[0m"
