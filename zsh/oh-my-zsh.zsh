4f8d90223c076b51ef3861b65c548215d4488f95
pooha302 <pooha302@gmail.com>
2026-02-25 19:07:14 +0900

    change oh-my-zsh plugin custom directory. add zsh-autosuggestions to zsh plugin

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,9 +1,10 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 export UPDATE_ZSH_DAYS=7
+export ZSH_CUSTOM=${ZDOTDIR}/custom
 
 ZSH_THEME="muse"
-plugins=(git fasd zsh-syntax-highlighting extract)
+plugins=(git fasd zsh-syntax-highlighting extract zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
